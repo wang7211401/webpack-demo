2a75eef4ecb2cf2863969e2bd688e8edcacee9e9
wang7211401 <wang7211401@163.com>
2017-11-26 11:15:05 +0800

    使用 webpack.config.js 和 npm scripts

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 import _ from 'lodash';
+import $ from 'jquery';
+
 function component(){
-    var element = document.createElement('div');
-    element.innerHTML = _.join(['Hello','webpack'],' ');
-    return element;
+    var element = $('<div></div>')
+    element.html(_.join(['Hello','webpack'],' '));
+    return element.get(0);
 }
 document.body.appendChild(component());
