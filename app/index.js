8353edda8e63f90b528a5d1435b6e867fe7a9446
wang7211401 <wang7211401@163.com>
2017-11-26 11:37:18 +0800

    “压缩js代码”

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import $ from 'jquery';
+import foo from './foo';
 
 function component(){
     var element = $('<div></div>')
@@ -7,3 +8,5 @@ function component(){
     return element.get(0);
 }
 document.body.appendChild(component());
+console.log(foo)
+console.log(foo())
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 var path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './app/index.js',
+  plugins: [
+    new UglifyJSPlugin({
+      test: /\.js($|\?)/i
+    })
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
