a31c4de13d1db78c5ef4ab8668c46ce4d1191e3d
wang7211401 <wang7211401@163.com>
2017-11-26 12:09:38 +0800

    使用 webpack-p 压缩

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,7 @@
 var path = require('path');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './app/index.js',
-  plugins: [
-    new UglifyJSPlugin({
-      test: /\.js($|\?)/i
-    })
-  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
