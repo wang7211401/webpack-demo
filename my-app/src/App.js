8da630d6e7b55486efe65298c4a340b2157ad0cd
wang7211401 <wang7211401@163.com>
2017-11-27 15:18:27 +0800

    normalize.css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,36 +6,36 @@ import TodoInput from "./TodoInput"
 import TodoItem from "./TodoItem"
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      newTodo:'test',
-      todoList:[
-        {id:1,title:"第一个待办"},
-        {id:2,title:"第二个待办"}
-      ]
+    constructor(props){
+        super(props)
+        this.state = {
+            newTodo:'test',
+            todoList:[
+                {id:1,title:"第一个待办"},
+                {id:2,title:"第二个待办"}
+            ]
+        }
     }
-  }
-  render() {
-    let todos = this.state.todoList.map((item,index) => {
-      return (
-        <li>
-          <TodoItem todo={item} />
+    render() {
+        let todos = this.state.todoList.map((item,index) => {
+                return (
+            <li>
+            <TodoItem todo={item} />
         </li>
-      )
+    )
     })
-    return (
-      <div className="App">
-        <h1>我的待办</h1>
-        <div className ="inputWraper">
-          <TodoInput content={this.state.newTodo} />
+        return (
+            <div className="App">
+            <h1>我的待办</h1>
+            <div className ="inputWraper">
+            <TodoInput content={this.state.newTodo} />
         </div>
         <ol>
-          {todos}
+        {todos}
         </ol>
-      </div>
+        </div>
     )
-  }
+    }
 }
 
 export default App;
--- my-app/src/TodoItem.js
+++ my-app/src/TodoItem.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 
 export default class TodoItem extends Component {
-  render(){
-    return <div>{this.props.todo.title}</div>
-  }
+    render(){
+        return <div>{this.props.todo.title}</div>
+    }
 }
