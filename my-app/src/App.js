14214d1cedaa0c09aa17fae3e149d5c81f0b8d52
wang7211401 <wang7211401@163.com>
2017-11-27 11:16:19 +0800

    添加 TodoInput

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import TodoInput from "./TodoInput"
 
 class App extends Component {
   constructor(props){
@@ -20,7 +20,7 @@ class App extends Component {
       <div className="App">
         <h1>我的待办</h1>
         <div className ="inputWraper">
-          <input type="text" value={this.state.newTodo} />
+          <TodoInput content={this.state.newTodo} />
         </div>
         <ol>
           {todos}
